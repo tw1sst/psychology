f2dddd51ce11e915409f3ba9a27d0efabcd6b15f
tw1sst <likovdenis12@gmail.com>
2024-10-15 00:49:33 +0300

    remove nuxt ui pro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/css/main.scss'],
-  extends: ['@nuxt/ui-pro'],
   modules: ['@nuxt/ui']
 })
